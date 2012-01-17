15076fea3e80cc960cc7315d8a9c89f1e43739b3
Sterling <me@mynameissterling.com>
2012-01-16 21:03:47 -0500

    application

---
+++ app/app.js
@@ -0,0 +1,360 @@
+var express = require('./node_modules/express');
+				
+var static = require('/usr/local/lib/node_modules/node-static');
+var jade = require('jade');
+var OAuth = require('oauth').OAuth;
+var url = require('url');
+var fs = require('fs');
+var http = require('http');
+var base64 = require('base64');
+
+// CSS ---------------------------------------------------
+var less = require('less');
+var parser = new(less.Parser)({
+    paths: ['.', './views/css'], // Specify search paths for @import directives
+    filename: 'style.less' // Specify a filename, for better error messages
+});
+
+parser.parse('.class { width: 1 + 1 }', function (e, tree) {
+    tree.toCSS({ compress: true }); // Minify CSS output
+});
+
+// DATABASE INFO -----------------------------------------
+var mysql = require('mysql'),
+	database = 'beer',
+	user_table = 'users',
+	client = mysql.createClient({ user: 'root', password: '' });
+	client.query('USE ' + database);
+
+// OAUTH SETUP --------------------------------------------
+var oa = new OAuth(
+	"https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"6nvxG75CDfoNWTiZ8jRQ",
+	"19cnrCnCH8C7bn7xxvXl1N4jVWPtIvTZjJ8zbBthSS0",
+	"1.0",
+	"http://localhost:1337/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+// var timestamp = Math.round((new Date()).getTime() / 1000);
+// var consumer_secret = '19cnrCnCH8C7bn7xxvXl1N4jVWPtIvTZjJ8zbBthSS0';
+// var oauth_token_secret = req.session.oauth.token_secret;
+// var composite_signing = consumer_secret + '&' + oauth_token_secret;
+// 
+// function signature(req, res, next) {
+// 	res.header()
+// }
+// var signature_base = encodeURIComponent();
+
+var app = express.createServer( 
+				express.static(__dirname + '/views'),
+				express.cookieParser(),
+				express.session({secret: 'FlurbleGurgleBurgle',
+				                store: new express.session.MemoryStore({ reapInterval: -1 }) }));
+
+function dateToString(date){ 
+	//check that date is a date object 
+	if (date && date.getFullYear()){ 
+		return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate() + ' ' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+	} 
+	return ""; 
+}
+
+process.on('uncaughtException', function(err) {
+	console.log('Caught exception: ' + err.stack);
+});
+
+app.use(express.cookieParser());
+app.use(express.compiler({ src: __dirname + '/views', enable: ['less'] }));
+app.use('/css', express.static(__dirname + '/views'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+function checkAuth(req, res, next) {
+	if (!req.session.user_name) {
+		res.redirect('/');
+	} else {
+		next();
+	}
+}
+
+app.get('/', function(req, res) {
+	res.render('index', { user_name: '', user_id: '', title: 'Stout' });
+});
+
+app.get('/dashboard', checkAuth, function(req, res) {
+	console.log(req.session.user_name);
+	res.render('dashboard', { user_name: req.session.user_name, user_id: req.session.user_id, title: 'Stout' });
+});
+
+app.get('/get-feed', checkAuth, function(req, res) {
+	client.query(
+		'SELECT DISTINCT feed.user_name, feed.user_id, users.avatar, feed.beer_id, feed.rating, feed.created_date, beers.name AS beer_name '
+		+ 'FROM feed, beers, users, followers '
+		+ 'WHERE ((feed.user_id = users.user_id) AND (feed.beer_id = beers.id)) AND (((followers.owner_id = feed.user_id) AND (followers.follower_id = ' + req.session.user_id + ')) '
+		+ 'OR ((feed.user_id = ' + req.session.user_id + '))) '
+		+ 'ORDER BY feed.created_date DESC LIMIT ' + req.query.limit + ',10',
+		function(err, sql_results, field) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send(sql_results);
+			}
+		});
+});
+
+app.get('/find-beer', checkAuth, function(req, res) {
+	console.log('search term: ' + req.query.beer_name);
+	client.query(
+		'SELECT beers.name, breweries.name AS brewery, beers.id, brewery_id, beers.description FROM beers, breweries WHERE (beers.brewery_id = breweries.id) AND beers.name LIKE "%' + req.query.beer_name + '%" LIMIT 0,10;',
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send(sql_results);
+			}
+		});
+});
+
+app.get('/beer-detail', checkAuth, function(req, res) {
+	client.query(
+		'SELECT beers.name, beers.description, beers.abv, beers.love, beers.like, beers.meh, beers.dislike, breweries.name AS brewery, categories.cat_name, styles.style_name, feed.rating FROM beers, breweries, categories, styles LEFT OUTER JOIN feed ON (feed.user_id = ' + req.session.user_id + ' AND feed.beer_id = ' + req.query.beer_id + ') WHERE (beers.id = ' + req.query.beer_id + ') AND (beers.brewery_id = breweries.id) AND (beers.cat_id = categories.id) AND (beers.style_id = styles.id)',
+		function(err, sql_results, field) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send(sql_results);
+			}
+		});
+});
+
+app.get('/beer-checkin', checkAuth, function(req, res) {
+	console.log('beerid: ' + req.query.beer_id);
+	var unrate = '';
+	if (req.query.unrate != '') {
+		var unrate = ', beers.' + req.query.unrate + ' = beers.' + req.query.unrate + ' - 1';
+		client.query('DELETE FROM feed WHERE (feed.user_id = ' + req.session.user_id + ') AND (feed.beer_id = ' + req.query.beer_id + ')');
+	}
+	client.query(
+		'UPDATE beers SET beers.' + req.query.rate + ' = beers.' + req.query.rate + ' + 1' + unrate + ' WHERE id = ' + req.query.beer_id + ';',
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send('{"status":"success"}');
+				var rate = '';
+				var time = new Date();
+				var now = dateToString(time);
+				switch(req.query.rate) {
+					case 'love':
+						rate = 1;
+						break;
+					case 'like':
+						rate = 2;
+						break;
+					case 'meh':
+						rate = 3;
+						break;
+					case 'dislike':
+						rate = 4;
+						break;
+				}
+				client.query(
+					'INSERT INTO feed ' +
+					'SET user_id = ?, user_name = ?, beer_id = ?, rating = ?, created_date = ?',
+					[req.session.user_id, req.session.user_name, req.query.beer_id, rate, now],
+					function(err, sql_results, fields) {
+						if (err) throw err;
+						if (sql_results != undefined) {
+							console.log(sql_results);
+							res.send('{"status":"success"}');
+						}
+					}
+				);
+			} else {
+				res.send('{"status":"failure"}');
+			}
+		});
+});
+
+app.get('/find-friend', checkAuth, function(req, res) {
+	console.log('search term: ' + req.query.user_name);
+	client.query(
+		'SELECT users.user_name, users.user_id, users.avatar, beers.name AS beer_name, beers.id FROM users, beers, feed WHERE (beers.id = feed.beer_id) AND (users.user_id = feed.user_id) AND users.user_name LIKE "%' + req.query.user_name + '%" ORDER BY users.created_date DESC LIMIT 0,10;',
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send(sql_results);
+			}
+		});
+});
+
+app.get('/get-profile', checkAuth, function(req, res) {
+	var user_id = req.session.user_id;
+	if (req.query.user_id != '') {
+		user_id = req.query.user_id;
+	}
+	console.log('profile for user id: ' + user_id);
+	client.query(
+		'SELECT users.user_name, users.avatar, users.user_id, feed.beer_id, feed.rating, beers.name AS beer_name, followers.created_date FROM users, feed, beers LEFT OUTER JOIN followers ON (follower_id = ' + req.session.user_id + ') AND (owner_id = ' + req.query.user_id + ') WHERE (users.user_id = ' + user_id + ') AND (feed.user_id = ' + user_id + ') AND (feed.beer_id = beers.id) ORDER BY feed.created_date DESC LIMIT 0,5;',
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined && sql_results != '') {
+				console.log(sql_results);
+				res.send(sql_results);
+			} else {
+				client.query(
+					'SELECT users.user_name, users.avatar, users.user_id FROM users WHERE users.user_id = ' + req.query.user_id,
+					function(err, sql_results, fields) {
+						if (err) throw err;
+						if (sql_results != undefined && sql_results != '') {
+							console.log(sql_results);
+							res.send(sql_results);
+						}
+					});
+			}
+		});
+});
+
+app.get('/follow', checkAuth, function(req, res) {
+	var time = new Date();
+	var now = dateToString(time);
+	client.query(
+		'INSERT INTO followers ' +
+		'SET owner_id = ?, follower_id = ?, created_date = ?',
+		[req.query.owner_id, req.session.user_id, now],
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send('{"status":"success"}');
+			}
+		});
+});
+
+app.get('/unfollow', checkAuth, function(req, res) {
+	var time = new Date();
+	var now = dateToString(time);
+	client.query(
+		'DELETE FROM followers WHERE (owner_id = ' + req.query.owner_id + ') AND (follower_id = ' + req.session.user_id + ')',
+		function(err, sql_results, fields) {
+			if (err) throw err;
+			if (sql_results != undefined) {
+				console.log(sql_results);
+				res.send('{"status":"success"}');
+			}
+		});
+});
+
+// app.get('/check-connection', checkAuth, function(req, res) {
+// 	var owner_id = '';
+// 	if (req.query.owner_id != '') {
+// 		owner_id = ' AND (owner_id = ' + req.query.user_id + ')';
+// 	}
+// 	client.query(
+// 		'SELECT created_date CASE WHEN (follower_id = ' + req.session.user_id + ')' + owner_id + ' THEN 1 ELSE 0 END AS following FROM followers',
+// 		function(err, sql_results, fields) {
+// 			if (err) throw err;
+// 			if (sql_results != undefined) {
+// 				console.log('follow? ' + sql_results);
+// 				res.send(sql_results);
+// 			}
+// 		}
+// 	);
+// });
+
+app.get('/auth/twitter', function(req, res){
+	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+		if (error) {
+			console.log(error);
+			res.send("yeah no. didn't work.");
+		} else {
+			req.session.oauth = {};
+			req.session.oauth.token = oauth_token;
+			console.log('oauth.token: ' + req.session.oauth.token);
+			req.session.oauth.token_secret = oauth_token_secret;
+			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+			res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
+		}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next) {
+	if (req.session.oauth) {
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+		var has_user = false;
+		
+		var time = new Date();
+		var now = dateToString(time);
+		
+		console.log('decode: ' + base64.encode(now)); // creates nonce;
+		
+		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+		function(error, oauth_access_token, oauth_access_token_secret, results) {
+			if (error) {
+				console.log(error);
+				res.send("yeah something broke.");
+			} else {
+				req.session.oauth.access_token = oauth_access_token;
+				req.session.oauth.access_token_secret = oauth_access_token_secret;
+				
+				// Check database for user
+				client.query(
+					'SELECT user_name FROM ' + user_table + ' WHERE user_name = "' + results.screen_name + '";',
+					function(err, sql_results, fields) {
+						if (err) throw err;
+						for(var i = 0; i < sql_results.length; i++) {
+							if (results.screen_name == sql_results[i].user_name) {
+								has_user = true;
+								console.log(results.screen_name + '==' + sql_results[i].user_name);
+							}
+						}
+						console.log('does user exist already? ' + has_user);
+						// Get user creation datetime
+						var time = new Date();
+						var now = dateToString(time);
+						var avatar = '';
+						// Checks if user is already in database
+						if (has_user) {
+							req.session.user_name = results.screen_name;
+							req.session.user_id = results.user_id;
+							console.log(now);
+							res.redirect('/dashboard');
+						} else {
+							console.log(now);
+							user_name = results.screen_name;
+							req.session.user_name = user_name;
+							req.session.user_id = results.user_id;
+							http.get({
+								host: 'api.twitter.com',
+								port: 80,
+								path: '/1/users/profile_image/' + results.screen_name + '.json' },
+								function(res) {
+									avatar = res.header('Location');
+									console.log(res.header('Location'));
+									client.query(
+										'INSERT INTO ' + user_table + ' ' +
+										'SET user_id = ?, user_name = ?, avatar = ?, access_token = ?, access_token_secret = ?, created_date = ?',
+										[results.user_id, results.screen_name, avatar, oauth_access_token, oauth_access_token_secret, now]
+									);
+								}
+							);
+							res.redirect('/dashboard');
+						}
+					});
+			}
+		});
+	} else {
+		next(new Error("you're not supposed to be here."));
+	}
+});
+
+app.get('/logout', function(req, res) {
+	delete req.session.user_name;
+	res.redirect('/');
+});
+
+app.listen(1337);
+console.log('Connected...');
---
+++ app/views/js/global.js
@@ -0,0 +1,412 @@
+var limit = 0;
+
+function message(message) {
+	$('body').append('<section id="overlay" onclick="$(\'#modal\').fadeOut();" style="width:100%; text-align:center; position:absolute; top:0; left:0; display:block; padding-top:75px; background:rgba(0,0,0,0.5);"><section style="background:#ffffff; color:#000000; display:block; width:150px; height:75px; margin:0 auto;">just testing</section><section style="clear:both;"></section></section>');
+}
+
+function tabSelect(name) {
+	$('html, body').animate({scrollTop: '0px'}, 0);
+	$('#footer ul li').removeClass('active');
+	if (name != '') {
+		$('#footer ul li.' + name).addClass('active');
+	} else if ($('ul#index').is(':visible')) { // not working
+		$('#footer ul li.the_tab').addClass('active');
+		loadTab(0);
+	}
+}
+
+function loadTab(limit) {
+	$('#index').empty().append('<li class="loader">Loading your tab...</li>');
+	tabSelect('the_tab');
+	
+	$.ajax({
+		cache: false,
+		time: 5000,
+		url: '/get-feed',
+		data: { limit: limit },
+		success: function(results) {
+					if (results != '') {
+						if (limit == 0) {
+							$('#index').empty();
+						}
+						for (var i = 0; i < results.length; i++) {
+							var action = '';
+							if (results[i].rating != '') {
+								switch(results[i].rating) {
+									case 1:
+										action = 'loves';
+										break;
+									case 2:
+										action = 'likes';
+										break;
+									case 3:
+										action = 'is whatever about';
+										break;
+									case 4:
+										action = 'dislikes';
+										break;
+								}
+							}
+							var avatar = '<img src="' + results[i].avatar + '" width="32px" class="left avatar" />'
+							var item_heading = '<p class="meta">' + results[i].user_name + ' ' + action + '</p>';
+							var beer_name = '<h3>' + results[i].beer_name + '</h3>';
+							$('#index').append('<li><a href="#profile" onclick="loadProfile(' + results[i].user_id + ')">'
+												+ '<section class="icon arrow right"></section>'
+												+ avatar
+												+ item_heading
+												+ beer_name
+												+ '</a></li>');
+						}
+						if (results.length > 5) {
+							$('#index').append('<li style="height:40px;"></li>');
+						}
+					} else {
+						$('#index').empty().append('<li class="loader">Your tab is empty, start by adding a beer and following others!</li>');
+					}
+				},
+		error: function(results) {
+					$('#index').empty().append('<li class="loader">There was a problem loading your tab!</li>');
+					return false;
+				}
+	});
+}
+
+function loadFindBeer() {
+	tabSelect('find_beer');
+	$('#beer_name').val('');
+	$('html, body').animate({scrollTop: '0px'}, 0);
+}
+
+function loadFindFriend() {
+	$('#user_name').val('');
+	$('html, body').animate({scrollTop: '0px'}, 0);
+}
+
+function loadProfile(id) {
+	$.ajax({
+		cache: false,
+		url: '/get-profile',
+		data: { user_id: id },
+		success: function(results) {
+					if (results != '') {
+						// Profile heading
+						var user_name = results[0].user_name;
+						if (results[0].user_id != $('#user_id').val()) {
+							var follow  = (results[0].created_date == null) ? '<li><a href="javascript:void(0);" onclick="follow(' + id + ');" class="btn orange ' + id + '">Follow</a></li>' : '<li><a href="javascript:void(0);" onclick="unfollow(' + id + ');" class="btn light ' + id + '">Unfollow</a></li>';
+							var profile_settings = '';
+						} else { // My profile
+							tabSelect('profile');
+							var follow = '';
+							var profile_settings = '<li><a href="mailto:me@mynameissterling.com" class="btn orange">Feedback</a><a href="/logout" class="btn orange">Logout</a><p class="meta ac">Made with love by MyNIS Labs</p><br /><br /><br /></li>'
+						}
+						$('ul#profile').attr('title',user_name).empty();
+						$('ul#profile').append('<li style="height:80px;">'
+											+ '<img src="' + results[0].avatar + '" width="48px" class="avatar left" />'
+											+ '<h3>' + user_name + '</h3>'
+											+ '<p class="meta">Last seen drinking a <b>' + results[0].beer_name + '</b></p>'
+											+ '</li>'
+											+ follow
+											+ '<li class="heading">Activity</li>');
+
+						// Activity stream
+						if (results[0].beer_name != null) {
+							for(var i = 0; i < results.length; i++) {
+								var action = '';
+								if (results[i].rating != '') {
+									switch(results[i].rating) {
+										case 1:
+											action = 'loves';
+											break;
+										case 2:
+											action = 'likes';
+											break;
+										case 3:
+											action = 'is whatever about';
+											break;
+										case 4:
+											action = 'dislikes';
+											break;
+									}
+								}
+								var item_heading = '<p class="meta">' + results[i].user_name + ' ' + action + '</p>';
+								var beer_name = '<h3>' + results[i].beer_name + '</h3>';
+								$('#profile').append('<li>'
+													+ '<a href="#beer_detail" onclick="beerDetail(\'' + results[i].beer_id + '\')">'
+													+ '<section class="icon arrow right"></section>'
+													+ item_heading
+													+ beer_name
+													+ '</a></li>');
+							}
+							$('#profile').append(profile_settings);
+						}
+						if (results.length > 5) {
+							$('#profile').append('<li style="height:40px;"></li>');
+						}
+					}
+				},
+		error: function(results) {
+					return false;
+				}
+	});
+}
+
+function follow(id) {
+	$.ajax({
+		cache: false,
+		url: '/follow',
+		data: { owner_id: id },
+		success: function(results) {
+					if (results != '') {
+						$('a.' + id).replaceWith('<a href="javascript:void(0);" onclick="unfollow(' + id + ');" class="btn light ' + id + '">Unfollow</a>');
+					} else {
+						console.log('There was an error!');
+					}
+				},
+		error: function(results) {
+					return false;
+				}
+	});
+}
+
+function unfollow(id) {
+	$.ajax({
+		cache: false,
+		url: '/unfollow',
+		data: { owner_id: id },
+		success: function(results) {
+					if (results != '') {
+						$('a.' + id).replaceWith('<a href="javascript:void(0);" onclick="follow(' + id + ');" class="btn orange ' + id + '">Follow</a>');
+					} else {
+						console.log('There was an error!');
+					}
+				},
+		error: function(results) {
+					return false;
+				}
+	});
+}
+
+function beerDetail(id) {
+	$.ajax({
+		cache: false,
+		url: '/beer-detail',
+		data: { beer_id: id },
+		success: function(results) {
+					if (results != '') {
+						var beer_name = escape(results[0].name);
+						var abv = (results[0].abv > 0) ? '<tr><th>ABV:</th><td>' + results[0].abv + '%</td></tr>' : '';
+						var category = (results[0].cat_name != '') ? '<tr><th>Category:</th><td>' + results[0].cat_name + '</td></tr>' : '';
+						var style = (results[0].style_name != '') ? '<tr><th>Style:</th><td>' + results[0].style_name + '</td></tr>' : '';
+						
+						var love = (results[0].love > 0) ? results[0].love : '';
+						var like = (results[0].like > 0) ? results[0].like : '';
+						var meh = (results[0].meh > 0) ? results[0].meh : '';
+						var dislike = (results[0].dislike > 0) ? results[0].dislike : '';
+						
+						var love_active = '', like_active = '', meh_active = '', dislike_active = '';
+						switch(results[0].rating) {
+							case 1:
+								love_active = ' active';
+								break;
+							case 2:
+								like_active = ' active';
+								break;
+							case 3:
+								meh_active = ' active';
+								break;
+							case 4:
+								dislike_active = ' active';
+								break;
+						}
+						
+						$('#beer_detail').empty().attr('title',unescape(beer_name)).append('<li><ul class="rate_controls ' + id + '">'
+															+ '<li onclick="rateBeer(' + id + ',\'love\');" class="love' + love_active + '"><p class="rate_count right">' + love + '</p><span></span></li>'
+															+ '<li onclick="rateBeer(' + id + ',\'like\');" class="like' + like_active + '"><p class="rate_count right">' + like + '</p><span></span></li>'
+															+ '<li onclick="rateBeer(' + id + ',\'meh\');" class="meh' + meh_active + '"><p class="rate_count right">' + meh + '</p><span></span></li>'
+															+ '<li onclick="rateBeer(' + id + ',\'dislike\');" class="dislike' + dislike_active + '"><p class="rate_count right">' + dislike + '</p><span></span></li>'
+															+ '</ul></li>'
+															+ '<li id="' + id + '"><table class="stats">'
+															+ abv
+															+ category
+															+ style
+															+ '</table></li>'
+															+ '<li style="display:none;"><a href="#index" onclick="loadTab();" class="btn orange">Beer Me</a></li>');
+					}
+				},
+		error: function() {
+					return false;
+				}
+	});
+	// var copy = $('ul#find_beer a#' + id).html();
+}
+
+function updateBeerCount(id,unrate) {
+	var uncount = parseInt($('ul.' + id + ' li.' + unrate + ' p.rate_count').text()) - 1;
+	if (uncount < 0) {
+		$('ul.' + id + ' li.' + unrate + ' p.rate_count').empty();
+	} else {
+		$('ul.' + id + ' li.' + unrate + ' p.rate_count').empty().append(uncount);
+	}
+}
+
+function rateBeer(id,rate) {
+	var unrate = '';
+	if ($('ul.' + id + ' li').hasClass('active')) {
+		if ($('ul.' + id + ' li.love').hasClass('active')) {
+			unrate = 'love';
+			updateBeerCount(id,unrate);
+		} else if ($('ul.' + id + ' li.like').hasClass('active')) {
+			unrate = 'like';
+			updateBeerCount(id,unrate);
+		} else if ($('ul.' + id + ' li.meh').hasClass('active')) {
+			unrate = 'meh';
+			updateBeerCount(id,unrate);
+		} else if ($('ul.' + id + ' li.dislike').hasClass('active')) {
+			unrate = 'dislike';
+			updateBeerCount(id,unrate);
+		}
+		$('ul.' + id + ' li').removeClass('active');
+	}
+	
+	$.ajax({
+		cahce: false,
+		url: '/beer-checkin',
+		data: { beer_id: id, rate: rate, unrate: unrate },
+		dataType: 'json',
+		success: function(results) {
+					if (results.status == 'success') {
+						var rate_count = $('ul.' + id + ' li.' + rate + ' p.rate_count');
+						$('ul.' + id + ' li.' + rate).addClass('active');
+						var count = parseInt(rate_count.text());
+						if (rate_count.is(':empty')) {
+							rate_count.append('1');
+						} else {
+							rate_count.empty().append(count + 1);
+						}
+					} else {
+						alert('There was an error!');
+					}
+				},
+		error: function(results) {
+					return false;
+				}
+	});
+}
+
+function findBeer() {
+	
+	var name = $.trim($('input#beer_name').val());
+	$('#find_beer span.results').empty().append('<li class="loader"><p class="meta">Brewing your search...</p></li>');
+	
+	if (name != '' && name.length > 1) {
+		$.ajax({
+			cache: false,
+			url: '/find-beer',
+			data: { beer_name: name },
+			dataType: 'json',
+			success: function(results) {
+						if (results != '') {
+							$('ul#find_beer span.results').empty();
+							for(var i = 0; i < results.length; i++) {
+								var id = results[i].id;
+								var brewery_id = results[i].brewery_id;
+								var beer_name = '<h3>' + results[i].name + '</h3>';
+								var brewery = '<p class="meta">' + results[i].brewery + '</p>';
+								$('ul#find_beer span.results').append('<li>'
+																	+ '<a href="#beer_detail" id="' + id + '" onclick="javascript:beerDetail(' + id + ');">'
+																	+ '<section class="icon arrow right"></section>'
+								 									+ beer_name
+																	+ brewery
+								 									+ '</a></li>');
+							}
+							if (results.length < 5) {
+								$('ul#find_beer span.results').append('<li><a href="#add_beer" class="ac btn orange" style="display:none;">Add new beer</a></li>');
+							} else {
+								$('ul#find_beer span.results').append('<li><a href="#add_beer" class="ac btn orange" style="display:none;">Add new beer</a></li><li style="height:40px;"></li>');
+							}
+						} else {
+							$('ul#find_beer span.results').empty().append('<li><a href="#add_beer" class="ac btn orange" style="display:none;">Add new beer</a></li>');
+						}
+					},
+			error: function(results) {
+						$('#find_beer span.results').empty().append('<li>There was a problem loading your tab!</li>');
+						return false;
+					}
+		});
+	}
+}
+
+function findFriend() {
+	
+	var name = $.trim($('input#user_name').val());
+	$('#find_friend span.results').empty().append('<li class="loader"><p class="meta">Brewing your search...</p></li>');
+	
+	if (name != '' && name.length > 1) {
+		$.ajax({
+			cache: false,
+			url: '/find-friend',
+			data: { user_name: name },
+			dataType: 'json',
+			success: function(results) {
+						if (results != '') {
+							$('ul#find_friend span.results').empty();
+							for(var i = 0; i < results.length; i++) {
+								var user_name = results[i].user_name;
+								var user_id = results[i].user_id;
+								var beer_name = '<h3>' + results[i].beer_name + '</h3>';
+								var avatar = '<p class="meta">' + results[i].avatar + '</p>';
+								$('ul#find_friend span.results').append('<li>'
+																	+ '<a href="#profile" id="' + user_id + '" onclick="javascript:loadProfile(' + user_id + ');">'
+																	+ '<section class="icon arrow right"></section>'
+								 									+ '<img src="' + results[0].avatar + '" width="32px" class="avatar left" />'
+																	+ '<h3>' + user_name + '</h3>'
+																	+ '<p class="meta">Last seen drinking a <b>' + results[0].beer_name + '</b></p>'
+								 									+ '</a></li>');
+							}
+							if (results.length > 5) {
+								$('ul#find_beer span.results').append('<li style="height:40px;"></li>');
+							}
+						} else {
+							$('ul#find_beer span.results').empty().append('<li class="loader"><p class="meta">No one by that name, try searching for another drinking buddy</p></li>');
+						}
+					},
+			error: function(results) {
+						$('#find_beer span.results').empty().append('<li>There was a problem loading your tab!</li>');
+						return false;
+					}
+		});
+	}
+}
+
+$(document).ready(function() {
+	$(function() {
+		if ($('#index[title="The Tab"]').is(':visible')) {
+			loadTab(0);
+		}
+	});
+	
+	$('#backButton').live('click',function() {
+		tabSelect('');
+	});
+	
+	// infinite scroll
+	$(window).scroll(function(){
+		var w = $(document).height() - $(window).height();
+		// var ww = w-300;
+		// Used to check the values:
+		// $('body').append('<div style="position:fixed; bottom: 250px; left: 5px; z-index:9999;;">'
+		//					  + '<input type="text" value="Window-50: ' + ww + '" />'
+		//					  + '<input type="text" value="Window Scroll: ' + $(window).scrollTop() + '" />');
+		if ($(window).scrollTop() >= w){
+			loadTab(limit);
+			limit += 10;
+		}
+	});
+	
+	$('input#beer_name').live('keyup', function() {
+		findBeer();
+	});
+	$('input#user_name').live('keyup', function() {
+		findFriend();
+	});
+});
