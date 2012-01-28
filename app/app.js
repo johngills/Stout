18c996a9db3588e645321edcd2060812d9bd93cc
Sterling <me@mynameissterling.com>
2012-01-28 17:00:03 -0500

    updated database settings

--- app/app.js
+++ app/app.js
@@ -65,19 +65,19 @@ function checkAuth(req, res, next) {
 }
 
 // DATABASE INFO -----------------------------------------
-// var mysql = require('mysql'),
-// 	database = 'stout',
-// 	user_table = 'users',
-// 	client = mysql.createClient({ user: 'sterlingrules', password: '@y&7~s45', host: 'mysql.mynameissterling.com', port: 3306 });
-// 	client.query('USE ' + database);
-// 	client.database = 'stout';
-
 var mysql = require('mysql'),
-	database = 'beer',
+	database = 'stout',
 	user_table = 'users',
-	client = mysql.createClient({ user: 'root', password: '' });
+	client = mysql.createClient({ user: 'sterlingrules', password: '@y&7~s45', host: 'mysql.mynameissterling.com', port: 3306 });
 	client.query('USE ' + database);
-	client.database = 'beer';
+	client.database = 'stout';
+
+// var mysql = require('mysql'),
+// 	database = 'beer',
+// 	user_table = 'users',
+// 	client = mysql.createClient({ user: 'root', password: '' });
+// 	client.query('USE ' + database);
+// 	client.database = 'beer';
 
 // OAUTH SETUP --------------------------------------------
 var oa = new OAuth(
@@ -86,8 +86,8 @@ var oa = new OAuth(
 	"Nmqm7UthsfdjaDQ4HcxPw",
 	"PIFvIPSXlTIbqnnnjBIqoWs0VIxpQivNrIJuWxtkLI",
 	"1.0",
-	"http://localhost:1337/auth/twitter/callback",
-	//"http://ps79519.dreamhostps.com:1337/auth/twitter/callback",
+	//"http://localhost:1337/auth/twitter/callback",
+	"http://ps79519.dreamhostps.com:1337/auth/twitter/callback",
 	"HMAC-SHA1"
 );
 
