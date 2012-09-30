2ad8ac7984c687ec7b26818e6fae843c617da27e
Sterling <me@mynameissterling.com>
2012-09-30 13:14:25 -0400

    tweaks

--- app/native_app.js
+++ app/native_app.js
@@ -1028,7 +1028,7 @@ app.get('/add-comment', checkAuth, function(req, res) {
 	client.query(
 		'BEGIN '
 		+ 'INSERT INTO comments (feed_id, owner_id, partner_id, beer_id, rating, comment, created_date) '
-		+ 'VALUES(' + req.query.feed_id + ', ' + req.query.owner_id + ', ' + req.query.user_id + ', ' + req.query.beer_id + ', ' + req.query.rating + ', ' + req.query.comment + ', ' + current + ')'
+		+ 'VALUES (' + req.query.feed_id + ', ' + req.query.owner_id + ', ' + req.query.user_id + ', ' + req.query.beer_id + ', ' + req.query.rating + ', "' + req.query.comment + '", ' + current + ') '
 		+ 'UPDATE feed SET feed.comment_count = feed.comment_count + 1 WHERE feed.id = ' + req.query.feed_id + ';'
 		+ 'COMMIT;',
 		function(err, results, fields) {
