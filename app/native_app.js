d16da4754b208c3672a0f63eff1f342d0ba384ea
Sterling <me@mynameissterling.com>
2012-10-09 23:35:05 -0400

    asdf

--- app/native_app.js
+++ app/native_app.js
@@ -1674,7 +1674,7 @@ app.get('/follow', checkAuth, function(req, res) {
 								        "message": results[0].full_name + " is now following you!",
 										"action": {
 													"type": "CUSTOM",
-													"data": { "acme1" : "bar", "acme2" : 42 },
+													"data":  "{ \"acme1\" : \"bar\", \"acme2\" : 42 }",
 										            //"type": "URL",
 										            //"data": "stout://dashboard.html?view_profile?" + req.query.user_id,
 										            "label": "View"
