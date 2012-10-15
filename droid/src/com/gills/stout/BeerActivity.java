cdca8c2168ae5e0a3fc0e89683d6c5d9bb0f2c85
john gills <john.gills@gmail.com>
2012-10-15 18:58:54 -0400

    still working on the dashboard params

--- droid/src/com/gills/stout/BeerActivity.java
+++ droid/src/com/gills/stout/BeerActivity.java
@@ -3,6 +3,7 @@ package com.gills.stout;
 import android.os.Bundle;
 import org.apache.cordova.*;
 import android.app.Activity;
+import android.content.Intent;
 
 
 public class BeerActivity extends DroidGap {
@@ -10,7 +11,15 @@ public class BeerActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        final Intent intent = new Intent("android.intent.action.MAIN");
+        final String args=intent.getDataString();
+        if(args == null)
+        {
+          super.loadUrl("file:///android_asset/www/index.html");
+        } else
+        {
+          super.loadUrl("file:///android_asset/www/index.html?args=" + args.split("//")[1]);
+        }
     }
 
 
